32b43b86192f74f21e298c26a65e60158bbc2749
DLionApps <darshanadilhan09@gmail.com>
2022-03-27 07:50:46 +0530

    initial commit

---
+++ IOTDeviceControl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IOTDeviceControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
